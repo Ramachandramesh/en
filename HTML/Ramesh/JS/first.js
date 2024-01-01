a8ab4ec945ce81e8d2ce043a3fdd75590b00501f
rama <Ramachandramesh>
2024-01-01 23:45:18 +0530

    add

---
+++ HTML/Ramesh/JS/first.js
@@ -0,0 +1,25 @@
+let x = 5;
+let y = 4;
+let z = x+y;
+/*console.log(z);
+let r = x+z;
+console.log(r);
+let s = 2**2;
+console.log(s);
+let k = s*2;
+console.log(k);
+console.log("Ramachandramesh Yadav");
+console.log('Ramachandramesh Yadav');
+console.log('56');
+console.log('rama'); */
+
+
+
+
+
+
+
+
+
+
+
